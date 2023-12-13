f02053730697d8cb31eef4c639c592c1f66d236a
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2023-12-12 19:08:45 -0500

    Make detoxifying codes simpler to run

--- Detoxifying_LM/detoxifying.py
+++ Detoxifying_LM/detoxifying.py
@@ -10,10 +10,8 @@ tqdm.pandas()
 model_name = "sshleifer/tiny-gpt2"
 model_save_path = f"output/{model_name.split('/')[-1]}_detoxified"
 dataset_name = "allenai/real-toxicity-prompts"
+input_size = 35
 
-device = "cuda:3" if torch.cuda.is_available() else "cpu"
-
-# TODO: Replace logging with wandb
 config = PPOConfig(
     model_name=model_name,
     learning_rate=1.47e-5,
@@ -27,8 +25,6 @@ config = PPOConfig(
 tokenizer = AutoTokenizer.from_pretrained(config.model_name)
 # GPT-2 / GPT-J tokenizer has a pad token, but it is not eos_token by default.
 tokenizer.pad_token = tokenizer.eos_token
-
-input_size = 35
 
 
 def collator(data):
@@ -57,9 +53,8 @@ dataset = dataset.train_test_split(test_size=0.2, shuffle=False)["train"]
 
 # set seed before initializing value head for deterministic eval
 set_seed(config.seed)
-# TODO: add , torch_dtype=torch.bfloat16 to config
-model = AutoModelForCausalLM.from_pretrained(config.model_name).to(device)
-model = AutoModelForCausalLMWithValueHead.from_pretrained(model).to(device)
+model = AutoModelForCausalLM.from_pretrained(config.model_name, torch_dtype=torch.bfloat16)
+model = AutoModelForCausalLMWithValueHead.from_pretrained(model)
 
 # We create a reference model by sharing 20 layers
 ref_model = create_reference_model(model, num_shared_layers=0)
@@ -76,14 +71,12 @@ ppo_trainer = PPOTrainer(
     data_collator=collator,
     optimizer=optimizer,
 )
-ppo_trainer.current_device = device
 
 # We then build the reward pipeline, we will use the toxicity model to compute the reward.
 # We first load the toxicity model and tokenizer.
-# TODO: add , torch_dtype=torch.float16 to config
 toxicity_model_id = "bert-base-uncased"
 toxicity_tokenizer = AutoTokenizer.from_pretrained(toxicity_model_id)
-toxicity_model = AutoModelForSequenceClassification.from_pretrained(toxicity_model_id).to(device)
+toxicity_model = AutoModelForSequenceClassification.from_pretrained(toxicity_model_id, torch_dtype=torch.float16).to(ppo_trainer.accelerator.device)
 
 # We then define the arguments to pass to the `generate` function
 generation_kwargs = {
@@ -95,7 +88,7 @@ generation_kwargs = {
     "max_new_tokens": input_size,
 }
 
-for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+for batch in tqdm(ppo_trainer.dataloader):
     query_tensors = batch["input_ids"]
 
     # Get response from the policy model
@@ -107,7 +100,7 @@ for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
 
     # Compute sentiment score
     texts = batch["response"]
-    toxicity_inputs = toxicity_tokenizer(texts, padding=True, truncation=True, return_tensors="pt").to(device)
+    toxicity_inputs = toxicity_tokenizer(texts, padding=True, truncation=True, return_tensors="pt").to(ppo_trainer.accelerator.device)
     logits = toxicity_model(**toxicity_inputs).logits.float()
     toxicity_labels = (logits[:, 0]).tolist()
 
