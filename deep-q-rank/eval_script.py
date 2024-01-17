73d122f859f793fb661d09fde3ba8a600dd3a0bd
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2024-01-17 02:19:27 -0500

    Make eval_script runnable

--- deep-q-rank/eval_script.py
+++ deep-q-rank/eval_script.py
@@ -5,23 +5,23 @@ from dqn import DQN, DQNAgent
 from tqdm import tqdm
 import os
 
-output_path = "output/dqn_model/eval_output.txt"
 data_folder = "/home/sajadeb/msmarco"
+bert_model_path = "/home/sajadeb/LLaMA_Debiasing/BiEncoder/output/bi-encoder_margin-mse_bert-base-uncased"
 model_save_path = "output/dqn_model/"
+output_file_path = os.path.join(model_save_path, "Run.txt")
+test_set_path = os.path.join(data_folder, "runbm25anserini.dev")
+pretrained_model_path = os.path.join(model_save_path, "model.pt")
+
 seed = 0
 epochs = 10
 window = 1
 top_docs_count = 10
-pretrained_model_path = "output/dqn_model/model.pt"
+ndcg_k_list = 10
+fold_list = [1, 10]
 
 
 def eval_model():
-    test_set_path = os.path.join(data_folder, "/runbm25anserini_top100_with_biases")
-    output_file_path = cfg.eval_output_file_path
-    fold_list = cfg.fold_list
-    ndcg_k_list = cfg.ndcg_k_list
-
-    test_set = load_dataset(test_set_path, top_docs_count)
+    test_set = load_dataset(test_set_path, bert_model_path, top_docs_count)
 
     model = DQN((768,), 1)
     model.load_state_dict(torch.load(pretrained_model_path))
@@ -30,10 +30,8 @@ def eval_model():
     for fold in tqdm(fold_list):
         ndcg_list = eval_agent_final(agent, ndcg_k_list, test_set)
         write_trec_results(agent, test_set, "relevance", output_file_path)
-        with open(output_path, "w") as f:
-            f.write("Fold {} NDCG Values: {}\n".format(fold, ndcg_k_list))
-            f.write(str(ndcg_list))
-            f.write("\n")
+        print(f"Fold {fold} NDCG Values: {ndcg_k_list}")
+        print(str(ndcg_list))
 
 
 if __name__ == "__main__":
