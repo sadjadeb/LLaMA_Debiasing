af5d21f869487c489a0c87a6a3d3fe21f1c14c5f
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2024-01-18 23:44:50 -0500

    Set hyperparameters

--- deep-q-rank/eval_script.py
+++ deep-q-rank/eval_script.py
@@ -8,27 +8,26 @@ import os
 data_folder = "/home/sajadeb/msmarco"
 bert_model_path = "/home/sajadeb/LLaMA_Debiasing/BiEncoder/output/bi-encoder_margin-mse_bert-base-uncased"
 model_save_path = "output/dqn_model/"
-output_file_path = os.path.join(model_save_path, "Run.txt")
-test_set_path = os.path.join(data_folder, "runbm25anserini.dev")
+test_set_path = os.path.join(data_folder, "runbm25anserini.dev_with_biases")
 pretrained_model_path = os.path.join(model_save_path, "model.pt")
 
 seed = 0
-epochs = 10
 window = 1
 top_docs_count = 10
 ndcg_k_list = 10
-fold_list = [1, 10]
+fold_list = [10]
 
 
 def eval_model():
-    test_set = load_dataset(test_set_path, bert_model_path, top_docs_count)
+    test_set = load_dataset(test_set_path, bert_model_path, top_docs_count, is_training=False)
 
-    model = DQN((768,), 1)
+    model = DQN((770,), 1)
     model.load_state_dict(torch.load(pretrained_model_path))
-    agent = DQNAgent((768,), learning_rate=3e-4, buffer=None, dataset=None, pre_trained_model=model)
+    agent = DQNAgent((770,), learning_rate=3e-4, buffer=None, dataset=None, pre_trained_model=model)
 
     for fold in tqdm(fold_list):
         ndcg_list = eval_agent_final(agent, ndcg_k_list, test_set)
+        output_file_path = os.path.join(model_save_path, f"Run_{fold}.txt")
         write_trec_results(agent, test_set, "relevance", output_file_path)
         print(f"Fold {fold} NDCG Values: {ndcg_k_list}")
         print(str(ndcg_list))
--- deep-q-rank/train_script.py
+++ deep-q-rank/train_script.py
@@ -8,25 +8,25 @@ from dqn import DQNAgent
 from mdp import BasicBuffer
 
 data_folder = "/home/sajadeb/msmarco"
-model_save_path = "output/dqn_model_1/"
+model_save_path = "output/dqn_model/"
 bert_model_path = "/home/sajadeb/LLaMA_Debiasing/BiEncoder/output/bi-encoder_margin-mse_bert-base-uncased"
 train_set_path = os.path.join(data_folder, "runbm25anserini_top100_with_biases")
 
 seed = 0
 epochs = 10
 window = 1
-top_docs_count = 1
+top_docs_count = 10
 
 
 def train_model():
     random.seed(seed)
 
-    train_set = load_dataset(train_set_path, bert_model_path, top_docs_count)
+    train_set = load_dataset(train_set_path, bert_model_path, top_docs_count, is_training=True)
 
     train_buffer = BasicBuffer(30000)
     train_buffer.push_batch(train_set, 3)
 
-    agent = DQNAgent((768,), learning_rate=3e-4, buffer=train_buffer, dataset=train_set)
+    agent = DQNAgent((770,), learning_rate=3e-4, buffer=train_buffer, dataset=train_set)
 
     y = []
     for _ in trange(epochs):
